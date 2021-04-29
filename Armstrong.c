a358b1ca0d513170f4187e61d1e8511beed00a04
Pooja Vasudev <poojavasudev.oct08@gmail.com>
2021-04-30 01:22:44 +0530

    Code-Armstrong

--- Armstrong.c
+++ Armstrong.c
@@ -3,13 +3,12 @@
 
 int main()
 {
-    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
     int n,sum,i,t,a;
     printf("\n\n\nThe Armstrong numbers in between 1 to 500 are : \n\n\n");
 
     for(i = 1; i <= 500; i++)
     {
-        t = i;  // as we need to retain the original number
+        t = i;  
         sum = 0;
         while(t != 0)
         {
@@ -21,7 +20,5 @@ int main()
         if(sum == i)
         printf("\n\t\t\t%d", i);
     }
-
-    printf("\n\n\n\n\t\t\tCoding is Fun !\n\n\n");
     return 0;
 }
